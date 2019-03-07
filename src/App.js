8d965cc31e5f48f06d3e4445d6844b23c87cf790
Mark Delk <jethrodaniel@gmail.com>
2019-03-06 19:33:58 -0600

    prefer functions over classes for simple components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Col,
   Container,
@@ -9,7 +9,6 @@ import {
 import {
   BrowserRouter as Router,
   Route,
-  Link,
   Switch
 } from 'react-router-dom';
 
@@ -17,39 +16,38 @@ import './App.css';
 
 import {Terminal} from './components/Terminal.js'
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path='/showme' component={() => window.location =
-             'http://jethrodaniel.com/showme'}/>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
-
-class HomePage extends Component {
-  render() {
-    return (
+const App = () => {
+  return (
+    <Router>
       <div>
-        <Navbar color="dark" dark expand="md"></Navbar>
-
-        <Container fluid={true} className="h-100">
-          <Row>
-            <Col md="3" className="d-none d-lg-block"></Col>
-            <Col id="terminal">
-              <Terminal value="Hello!"/>
-            </Col>
-            <Col md="3" className="d-none d-lg-block"></Col>
-          </Row>
-        </Container>
-
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path='/showme' component={() =>
+            window.location ='https://jethrodaniel.com/showme'
+          }/>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md"></Navbar>
+
+      <Container fluid={true} className="h-100">
+        <Row>
+          <Col md="3" className="d-none d-lg-block"></Col>
+          <Col id="terminal">
+            <Terminal value="Hello!"/>
+          </Col>
+          <Col md="3" className="d-none d-lg-block"></Col>
+        </Row>
+      </Container>
+
+    </div>
+  );
+}
+
+export default App;
