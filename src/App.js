ecdf569173bf3f683ae6c612a61625e4efabc26e
Mark Delk <jethrodaniel@gmail.com>
2019-05-23 21:44:32 -0500

    start on resume page

--- src/App.js
+++ src/App.js
@@ -22,24 +22,17 @@ const App = () => {
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path='/resume' component={ResumePage} />
         </Switch>
       </div>
     </Router>
   );
 }
 
-const Header = () => {
-  return (
-    <div>
-      <Navbar color="dark" dark expand="md"/>;
-    </div>
-  );
-}
-
 const HomePage = () => {
   return (
-    <div>
-      <Header/>
+    <div id="home-page">
+      <Navbar color="dark" dark expand="md"/>;
       <Container fluid={true} className="h-100">
         <Row>
           <Col md="3" className="d-none d-lg-block"></Col>
@@ -51,6 +44,23 @@ const HomePage = () => {
       </Container>
     </div>
   );
+}
+
+const ResumePage = () => {
+  return (
+    <div id="resume-page">
+      <Container fluid={true} className="h-100">
+        <Row>
+          <Col md="3" className="d-none d-lg-block"></Col>
+          <Col id="">
+            <p>Hello! Lorem Ipsum.</p>
+          </Col>
+          <Col md="3" className="d-none d-lg-block"></Col>
+        </Row>
+      </Container>
+    </div>
+  );
+
 }
 
 export default App;
