bffda2defedc986beb027556d30dfbd0a3917d59
Mark Delk <jethrodaniel@gmail.com>
2019-05-25 11:11:15 -0500

    split up pages files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import React from 'react';
-import {
-  Col,
-  Container,
-  Navbar,
-  NavbarBrand,
-  Row
-} from 'reactstrap';
 
 import {
   BrowserRouter as Router,
@@ -15,7 +8,8 @@ import {
 
 import './App.css';
 
-import {Terminal} from './components/Terminal.js'
+import Resume from './components/pages/resume/Resume.js'
+import HomePage from './components/pages/home/Home.js'
 
 const App = () => {
   return (
@@ -23,7 +17,7 @@ const App = () => {
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path='/resume' component={ResumePage} />
+          <Route exact path='/resume' component={Resume} />
           <Route component={NoMatch} />
         </Switch>
       </div>
@@ -36,40 +30,5 @@ const NoMatch = ({location}) => (
     <h3>No match for <code>{location.pathname}</code></h3>
   </div>
 );
-
-const HomePage = () => {
-  return (
-    <div id="home-page">
-      <Navbar color="dark" dark expand="md"/>;
-      <Container fluid={true} className="h-100">
-        <Row>
-          <Col md="3" className="d-none d-lg-block"></Col>
-          <Col id="terminal">
-            <Terminal value="Hello!"/>
-          </Col>
-          <Col md="3" className="d-none d-lg-block"></Col>
-        </Row>
-      </Container>
-    </div>
-  );
-}
-
-const ResumePage = () => {
-  return (
-    <div id="resume-page">
-      <Navbar color="light" expand="md"/>
-      <Container fluid={true} className="h-100">
-        <Row>
-          <Col md="3" className="d-none d-lg-block"></Col>
-          <Col id="">
-            <p>Hello! Lorem Ipsum.</p>
-          </Col>
-          <Col md="3" className="d-none d-lg-block"></Col>
-        </Row>
-      </Container>
-    </div>
-  );
-
-}
 
 export default App;
