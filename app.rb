e45debff5db5d7997d48c7669676bc411e08102b
Mark Delk <jethrodaniel@gmail.com>
2019-07-07 23:17:57 -0400

    start getting ruby classes from js

--- app.rb
+++ app.rb
@@ -13,41 +13,41 @@ end
 require 'native'
 require 'react/umd/react.production.min.js'
 require 'react-dom/umd/react-dom.production.min.js'
+require 'react-router-dom/umd/react-router-dom.min.js'
 
 require 'opal'
 require 'opal-jquery'
 require 'jquery/dist/jquery.min.js'
 
-def add_basic_react_component!
-  `
-    'use strict';
-
-    class LikeButton extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = { liked: false };
-      }
-
-      render() {
-        if (this.state.liked)
-          return 'You liked this.';
-
-        return React.createElement(
-          'button',
-          { onClick: () => this.setState({ liked: true }) },
-          'Like'
-        );
-      }
+<<~`CODE`
+  class LikeButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { liked: false };
     }
 
+    render() {
+      if (this.state.liked)
+        return 'You liked this.';
+
+      return React.createElement(
+        'button',
+        { onClick: () => this.setState({ liked: true }) },
+        'Like'
+      );
+    }
+  }
+CODE
+
+def render!
+  %x{
     ReactDOM.render(
-      React.createElement(LikeButton),
+      React.createElement(#{`LikeButton`}),
       document.querySelector('#like_button_container')
     );
-
-    `
+  }
 end
 
 Document.ready? do
-  add_basic_react_component!
+  render!
 end
