10ab7a1af78e5ced4ad7ef0ea5cdcfda5f007423
Mark Delk <jethrodaniel@gmail.com>
2020-03-24 20:29:31 -0500

    tmp, wepack config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// see https://webpack.js.org/configuration/
+
+const path = require('path');
+
+module.exports = {
+  // mode: "production",
+  mode: "development",
+  devtool: "source-map",
+  // output: {
+    // filename: 'bundle.js'
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: ['style-loader','css-loader', 'sass-loader']
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.coffee$/,
+        use: [ 'coffee-loader' ]
+      }
+    ]
+  },
+  plugins: [
+  ]
+};
